15d02d9bb058f2211ce91d09172ad7ef96a46880
Greg Pfeil <greg@technomadic.org>
2025-07-06 19:13:18 -0600

    Initial commit

---
+++ .config/project/default.nix
@@ -0,0 +1,47 @@
+{
+  config,
+  flaky,
+  lib,
+  pkgs,
+  supportedSystems,
+  ...
+}: {
+  project = {
+    name = "hpack-dhall-defaults";
+    summary = "hpack-dhall configuration shared across all of my projects";
+
+    devPackages = [
+      pkgs.dhall
+      pkgs.dhall-docs
+      pkgs.dhall-lsp-server
+    ];
+  };
+
+  ## dependency managerment
+  services.renovate.enable = true;
+
+  ## development
+  programs = {
+    direnv.enable = true;
+    # This should default by whether there is a .git file/dir (and whether it’s
+    # a file (worktree) or dir determines other things – like where hooks
+    # are installed.
+    git.enable = true;
+  };
+
+  ## formatting
+  editorconfig.enable = true;
+  programs = {
+    treefmt.enable = true;
+    vale.enable = true;
+  };
+  project.file.".dir-locals.el".source = ../emacs/.dir-locals.el;
+
+  ## CI
+  services.garnix.enable = true;
+
+  ## publishing
+  services.flakehub.enable = true;
+  services.github.enable = true;
+  services.github.settings.repository.topics = ["library"];
+}
---
+++ flake.nix
@@ -0,0 +1,113 @@
+{
+  description = "hpack-dhall configuration shared across all of my projects";
+
+  nixConfig = {
+    ## https://github.com/NixOS/rfcs/blob/master/rfcs/0045-deprecate-url-syntax.md
+    extra-experimental-features = ["no-url-literals"];
+    extra-substituters = [
+      "https://cache.dhall-lang.org"
+      "https://cache.garnix.io"
+      "https://dhall.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "cache.dhall-lang.org:I9/H18WHd60olG5GsIjolp7CtepSgJmM2CsO813VTmM="
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+      "dhall.cachix.org-1:8laGciue2JBwD49ICFtg+cIF8ddDaW7OFBjDb/dHEAo="
+    ];
+    ## Isolate the build.
+    sandbox = "relaxed";
+    use-registries = false;
+  };
+
+  outputs = {
+    flake-utils,
+    flaky,
+    hall,
+    nixpkgs,
+    self,
+    systems,
+  }: let
+    pname = "hpack-dhall-defaults";
+
+    supportedSystems = import systems;
+  in
+    {
+      schemas = {
+        inherit
+          (flaky.schemas)
+          overlays
+          homeConfigurations
+          packages
+          devShells
+          projectConfigurations
+          checks
+          formatter
+          ;
+      };
+
+      overlays = {
+        default = final: prev: {
+          dhallPackages = prev.dhallPackages.override (old: {
+            overrides =
+              final.lib.composeExtensions
+              (old.overrides or (_: _: {}))
+              (self.overlays.dhall final prev);
+          });
+        };
+
+        dhall = final: prev: dfinal: dprev: {
+          ${pname} = self.packages.${final.system}.${pname};
+        };
+      };
+
+      homeConfigurations =
+        builtins.listToAttrs
+        (builtins.map
+          (flaky.lib.homeConfigurations.example self
+            ## TODO: Is there something more like `dhallWithPackages`?
+            [({pkgs, ...}: {home.packages = [pkgs.dhallPackages.${pname}];})])
+          supportedSystems);
+    }
+    // flake-utils.lib.eachSystem supportedSystems (system: let
+      pkgs = nixpkgs.legacyPackages.${system}.appendOverlays [
+        flaky.overlays.default
+        hall.overlays.default
+      ];
+
+      src = pkgs.lib.cleanSource ./.;
+    in {
+      packages = {
+        default = self.packages.${system}.${pname};
+
+        "${pname}" = pkgs.checkedDrv (pkgs.dhallPackages.buildDhallDirectoryPackage {
+          src = "${src}/dhall";
+          name = pname;
+          dependencies = [pkgs.dhallPackages.hall];
+          document = true;
+        });
+      };
+
+      projectConfigurations =
+        flaky.lib.projectConfigurations.dhall {inherit pkgs self;};
+
+      devShells =
+        self.projectConfigurations.${system}.devShells
+        // {default = flaky.lib.devShells.default system self [] "";};
+      checks = self.projectConfigurations.${system}.checks;
+      formatter = self.projectConfigurations.${system}.formatter;
+    });
+
+  inputs = {
+    ## Flaky should generally be the source of truth for its inputs.
+    flaky.url = "github:sellout/flaky";
+
+    flake-utils.follows = "flaky/flake-utils";
+    nixpkgs.follows = "flaky/nixpkgs";
+    systems.follows = "flaky/systems";
+
+    hall = {
+      inputs.flaky.follows = "flaky";
+      url = "github:sellout/hall";
+    };
+  };
+}
