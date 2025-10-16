43d6eb5a6178759239d20becc247978d941a55ce
Greg Pfeil <greg@technomadic.org>
2025-10-16 17:34:52 -0600

    Sync Flaky template

--- flake.nix
+++ flake.nix
@@ -2,17 +2,22 @@
   description = "hpack-dhall configuration shared across all of my projects";
 
   nixConfig = {
+    ## NB: This is a consequence of using `self.pkgsLib.runEmptyCommand`, which
+    ##     allows us to sandbox derivations that otherwise can’t be.
+    allow-import-from-derivation = true;
     ## https://github.com/NixOS/rfcs/blob/master/rfcs/0045-deprecate-url-syntax.md
     extra-experimental-features = ["no-url-literals"];
     extra-substituters = [
       "https://cache.dhall-lang.org"
       "https://cache.garnix.io"
       "https://dhall.cachix.org"
+      "https://sellout.cachix.org"
     ];
     extra-trusted-public-keys = [
       "cache.dhall-lang.org:I9/H18WHd60olG5GsIjolp7CtepSgJmM2CsO813VTmM="
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       "dhall.cachix.org-1:8laGciue2JBwD49ICFtg+cIF8ddDaW7OFBjDb/dHEAo="
+      "sellout.cachix.org-1:v37cTpWBEycnYxSPAgSQ57Wiqd3wjljni2aC0Xry1DE="
     ];
     ## Isolate the build.
     sandbox = "relaxed";
